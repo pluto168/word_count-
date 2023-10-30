b91e87282da5b93d62c847980a320b7b5551bbb8
pluto168 <86468019+pluto168@users.noreply.github.com>
2023-10-30 19:16:14 +0800

    count line word letter

--- try.py
+++ try.py
@@ -1,5 +1,4 @@
 from sys import argv
-#print(argv)
 
 if len(argv) < 2:
     print("Please provide a filename.")
@@ -7,5 +6,19 @@ if len(argv) < 2:
 else:
     file = open(argv[1])
     lines = file.read()
-    print(type(lines))
-    print(lines)
+    
+    #print(lines) #a list of strings
+    lines = lines.split("\n")
+    word_count = 0
+    letter_count = 0
+    
+    for line in lines:
+        words = line.split(" ")
+        word_count += len(words)
+        letter_count += len(line)
+    
+    line_count = len(lines) - 1
+    
+    print(f"The line count is {line_count}")
+    print(f"The word count is {word_count}")
+    print(f"The letter count is {letter_count}")
